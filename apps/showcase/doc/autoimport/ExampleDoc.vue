93b223211cfbacfa53db42db78525c60ebb7cf28
njevric <njevric9@gmail.com>
2026-08-05 00:16:29 +0200

    chore(showcase): remove the getting started playground route

--- apps/showcase/doc/autoimport/ExampleDoc.vue
+++ apps/showcase/doc/autoimport/ExampleDoc.vue
@@ -1,5 +1,8 @@
 <template>
     <DocSectionText v-bind="$attrs">
-        <p>A complete example using OpenVue with auto imports is available at the <NuxtLink to="/playground">playground</NuxtLink>.</p>
+        <p>
+            A complete example using OpenVue with auto imports is available on
+            <a href="https://stackblitz.com/edit/vitejs-vite-nxnmay?file=src%2FApp.vue" target="_blank" rel="noopener noreferrer">StackBlitz</a>.
+        </p>
     </DocSectionText>
 </template>
